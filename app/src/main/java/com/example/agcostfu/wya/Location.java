cccfec0e08064182ccecdc954ba6a6b1893c5fba
Harjit <harjit.randhawa.1122@gmail.com>
2015-05-05 01:26:19 -0700

    merged shit with android

---
+++ app/src/main/java/com/example/agcostfu/wya/Location.java
@@ -0,0 +1,100 @@
+package com.example.agcostfu.wya;
+
+public class Location {
+    private int _id;
+    private double _lat;
+    private double _lng;
+    private String name;
+    private String address;
+    private String date;
+
+    public Location() {
+
+    }
+
+    public Location(int id, double lat, double lng) {
+        this._id = id;
+        this._lat = lat;
+        this._lng = lng;
+    }
+
+    public Location(double lat, double lng) {
+        this._lat = lat;
+        this._lng = lng;
+    }
+
+    public Location(double lat, double lng, String date){
+        this._lat = lat;
+        this._lng = lng;
+        this.date = date;
+    }
+
+    // constructor to generate all the SQLite parameters for a new location
+    public Location(int id, double lat, double lng,
+                    String name, String address, String date) {
+        this._id = id;
+        this._lat = lat;
+        this._lng = lng;
+        this.name = name;
+        this.address = address;
+        this.date = date;
+    }
+
+    // constructor to generate all the SQLite parameters for a new location
+    public Location(double lat, double lng,
+                    String name, String address, String date) {
+        this._lat = lat;
+        this._lng = lng;
+        this.name = name;
+        this.address = address;
+        this.date = date;
+    }
+
+    public void setID(int id) {
+        this._id = id;
+    }
+
+    public int getID() {
+        return this._id;
+    }
+
+    public void setLatitude(double lat) {
+        this._lat = lat;
+    }
+
+    public double getLatitude() {
+        return this._lat;
+    }
+
+    public void setLongitude(double lng) {
+        this._lng = lng;
+    }
+
+    public double getLongitude() {
+        return this._lng;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+}
--- app/src/main/java/com/example/agcostfu/wya/MainMapsActivity.java
+++ app/src/main/java/com/example/agcostfu/wya/MainMapsActivity.java
@@ -1,66 +1,184 @@
 package com.example.agcostfu.wya;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.annotation.TargetApi;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
+import static com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.String;
+
+import com.example.agcostfu.wya.GPSTracker;
+import  com.example.agcostfu.wya.Location;
+import com.example.agcostfu.wya.SplashScreen;
+
+
 
 
 public class MainMapsActivity extends ActionBarActivity {
 
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    GPSTracker gps;
+    static LatLng nearest;
+    static String name;
+    double radius = 5.0;
+    double lat;
+    double lng;
+
+    static TextView textView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_maps);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        //SplashScreen splash = new SplashScreen();
+        gps = new GPSTracker(MainMapsActivity.this);
+        lat = gps.getLocation().getLatitude();
+        lng = gps.getLocation().getLongitude();
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_maps, menu);
-        return true;
+        setUpMapIfNeeded();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+// Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+// Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+// Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * This is where we can add markers or lines, add listeners or move the camera
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-    public static class PlaceholderFragment extends Fragment {
+    private void setUpMap() {
+        LatLng current = new LatLng(gps.getLocation().getLatitude(),gps.getLocation().getLongitude());
 
-        public PlaceholderFragment() {
-        }
+        mMap.addMarker(new MarkerOptions()
+                .position(current)
+                .title("USER 1")
+                .snippet("This is User 1's current location")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main_maps, container, false);
-            return rootView;
-        }
+        mMap.setMyLocationEnabled(true);
+        //this sets the view of the map zoomed such that the user's current
+        //location and the second users location is visible.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 12));
+
+        mMap.getMyLocation();
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        // This addMarker sets the location of the second User in Downtown SF.
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.773972, -122.431297))
+                .title("USER 2")
+                .snippet("This is User 2's Current Position.")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        Toast.makeText(getApplicationContext(),
+                "WELCOME!",
+                Toast.LENGTH_LONG).show();
+
+        // This set the function for long press on the map.
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng)
+                        .title("Your location : ")
+                        .snippet("lat : " + latLng.latitude + "\nlng : " + latLng.longitude)
+                        .draggable(true));
+                lat = latLng.latitude;
+                lng = latLng.longitude;
+                Toast.makeText(getApplicationContext(),
+                        "NEW MARKER SET!",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        mMap.setOnMarkerDragListener(new OnMarkerDragListener() {
+                                         @Override
+                                         public void onMarkerDragStart(Marker marker) {
+                                         }
+
+                                         @Override
+                                         public void onMarkerDrag(Marker marker) {
+
+                                         }
+
+                                         @Override
+                                         public void onMarkerDragEnd(Marker marker) {
+                                             mMap.clear();
+                                             LatLng latLng = marker.getPosition();
+                                             mMap.addMarker(new MarkerOptions().position(latLng)
+                                                     .title("Your location : ")
+                                                     .draggable(true)
+                                                     .snippet("lat : " + latLng.latitude + "\nlng : " + latLng.longitude));
+                                             lat = latLng.latitude;
+                                             lng = latLng.longitude;
+                                             Toast.makeText(getApplicationContext(),
+                                                     "Lat : " + latLng.latitude + " ",
+                                                     Toast.LENGTH_LONG).show();
+                                         }
+                                     }
+
+        );
+
+
+    }
+
+
+    //Creates the menu options in the action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_maps, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
